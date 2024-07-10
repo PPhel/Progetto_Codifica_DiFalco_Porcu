c3d80c6c27890ec897d6d36f88a270b4611a9b17
Luca Porcu <163210712+lucap00@users.noreply.github.com>
2024-07-10 15:54:13 +0200

    varie modifiche

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-$(".correzioni").click(function () {
-    $(this).toggleClass('active_leg');
-    $("#corr").toggle();
-    $("#corr").css({ "background-color": "purple", "color": "white" });
-});
+document.addEventListener('DOMContentLoaded', function() {
+    // Funzione per scrollare alla sezione delle correzioni
+    document.getElementById('correzioni').addEventListener('click', function() {
+      document.getElementById('corr').style.color = "red";
+    });
+  
+    // Funzione per scrollare alla sezione delle parole particolari
+    document.getElementById('paroleparticolari').addEventListener('click', function() {
+      document.getElementById('orig').scrollIntoView({ behavior: 'smooth' });
+    });
+  
+    // Funzione per scrollare alla sezione delle persone
+    document.getElementById('persone').addEventListener('click', function() {
+      document.getElementBy('persName').scrollIntoView({ behavior: 'smooth' });
+    });
+  
+    // Funzione per scrollare alla sezione dei luoghi
+    document.getElementById('luoghi').addEventListener('click', function() {
+      document.getElementById('placeName').scrollIntoView({ behavior: 'smooth' });
+    });
+  
+    // Funzione per scrollare alla sezione delle organizzazioni
+    document.getElementById('org').addEventListener('click', function() {
+      document.getElementById('orgName').scrollIntoView({ behavior: 'smooth' });
+    });
+  });
