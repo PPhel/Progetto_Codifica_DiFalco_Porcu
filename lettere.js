381bf0ad1f38c8aa1a2d2de1f74424f98ed55f98
Luca Porcu <163210712+lucap00@users.noreply.github.com>
2024-07-10 18:41:16 +0200

    varie modifiche

--- lettere.js
+++ lettere.js
@@ -1,5 +1,5 @@
+//gestione dei pulsanti
 document.addEventListener('DOMContentLoaded', function() {
-    // Funzione per scrollare alla sezione delle correzioni
     document.getElementById('correzioni').addEventListener('click', function() {
         const correzioni = document.getElementsByClassName("corr");
         for(i=0;i<correzioni.length;i++){
@@ -49,3 +49,22 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
   });
+
+  //gestore foto
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("imgDiario");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
